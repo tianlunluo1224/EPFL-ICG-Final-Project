a6ac09445515477d67ea79bd83a7fd19858f4e84
Stefan Rickli <stefan.rickli@epfl.ch>
2019-05-19 12:48:06 +0200

    Remove time_step() commented code

--- 3D_inverse_kinematics_0/src/object/object.h
+++ 3D_inverse_kinematics_0/src/object/object.h
@@ -59,12 +59,8 @@ public:
         return mat4(base_orientation_);
     }
 
-    /// set the time for every update
     virtual void time_step(float _time)
-    {
-        // angle_orbit_ = fmod(angle_orbit_ + _days * angle_step_orbit_, 360.0);
-        // angle_self_  = fmod(angle_self_  + _days * angle_step_self_,  360.0);
-    }
+    {}
 
     virtual void update_dof(std::vector<float> values)
     {}
