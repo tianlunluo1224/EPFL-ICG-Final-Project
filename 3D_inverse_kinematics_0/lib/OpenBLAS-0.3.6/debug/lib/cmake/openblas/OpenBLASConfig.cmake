3a196beca95cc3100ab0c4f39d12fd52e5934cb3
Stefan Rickli <stefan.rickli@epfl.ch>
2019-05-21 18:54:36 +0200

    Add OpenBLAS-0.3.6

---
+++ 3D_inverse_kinematics_0/lib/OpenBLAS-0.3.6/debug/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.7.dev")
+SET(OpenBLAS_INCLUDE_DIRS /home/stefan/OpenBLAS_export/include)
+SET(OpenBLAS_LIBRARIES /home/stefan/OpenBLAS_export/bin/libopenblas.dll)
---
+++ 3D_inverse_kinematics_0/lib/OpenBLAS-0.3.6/release/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.7.dev")
+SET(OpenBLAS_INCLUDE_DIRS /home/stefan/OpenBLAS-0.3.6_release/include)
+SET(OpenBLAS_LIBRARIES /home/stefan/OpenBLAS-0.3.6_release/bin/libopenblas.dll)
