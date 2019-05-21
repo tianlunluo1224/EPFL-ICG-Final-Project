39d3bb23d3585925c1ebdacbfc267716c44e6dda
Stefan Rickli <stefan.rickli@epfl.ch>
2019-05-21 21:40:25 +0200

    Remove Math_Objects

--- 3D_inverse_kinematics_0/src/kinematics.h
+++ 3D_inverse_kinematics_0/src/kinematics.h
@@ -51,36 +51,6 @@ protected:
 };
 
 
-class Math_Object {
-public:
-    virtual std::pair<vec4, mat4> forward(std::pair<vec4, mat4> _prev_coordinates, std::vector<float> _state)
-    {
-        return _prev_coordinates;
-    }
-};
-
-class Math_Hinge : public Math_Object {
-public:
-    std::pair<vec4, mat4> forward(std::pair<vec4, mat4> _prev_coordinates, std::vector<float> _state) {
-        assert(!_state.empty());
-        return std::pair<vec4, mat4>(_prev_coordinates.first,
-                                     mat4::rotate_x(_state.at(0)) * _prev_coordinates.second);
-    }
-};
-
-class Math_Bone : public Math_Object {
-public:
-    float length_;
-
-public:
-    Math_Bone(float _length) : length_(_length) {}
-
-    std::pair<vec4, mat4> forward(std::pair<vec4, mat4> _prev_coordinates, std::vector<float> _state) {
-        return std::pair<vec4, mat4>(mat4::translate(length_ * _prev_coordinates.second.base_z()) * _prev_coordinates.first,
-                                     _prev_coordinates.second);
-    }
-};
-
 //=============================================================================
 #endif
 //=============================================================================
